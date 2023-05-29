ad517999125c4895963f271aa7f9031bf55a8db6
elias <eliasey11@gmail.com>
2023-05-29 18:06:13 +0300

    firebase sign in with email done

---
+++ firebase-swiftUI/Authentication/AuthenticationManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationManager.swift
+//  firebase-swiftUI
+//
+//  Created by Elias Ayele on 29/05/2023.
+//
+
+import Foundation
---
+++ firebase-swiftUI/Authentication/SignInEmailView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInEmailView.swift
+//  firebase-swiftUI
+//
+//  Created by Elias Ayele on 29/05/2023.
+//
+
+import SwiftUI
+
+struct SignInEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInEmailView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInEmailView()
+    }
+}
